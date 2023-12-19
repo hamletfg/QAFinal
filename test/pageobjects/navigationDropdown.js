03b9ab1459e2a2b5dafcbcb000a9521dfbeabdd1
Hamlet Gordillo <hamletfg@gmail.com>
2023-12-19 14:06:49 -0700

    adding code to navDropDown file

--- test/pageobjects/navigationDropdown.js
+++ test/pageobjects/navigationDropdown.js
@@ -12,6 +12,34 @@ class NavigationDropdown extends OpenURL {
         return $('//li[@id=5]')
     }
 
+    get laptops () {
+        return $('div[automation-id="categoryLaptops"]')
+    }
+
+    get macBookImg () {
+        return $('//img[contains(@src, "mac-os.jpg")]')
+    }
+
+    get macBook () {
+        return $('a[automation-id="productDescriptionLink_10"]')
+    }
+
+    get increaseButton () {
+        return $('button[automation-id="increaseButton_10"]')
+    }
+
+    get decreaseError () {
+        return $('//span[@id="errordiv-10"][contains(text(), "Quantity")]')
+    }
+
+    async dropdownMacbookPro () {
+        await this.navDropdownBtn.click()
+        await this.dropdownComputer.click()
+        await this.laptops.click()
+        await this.macBookImg.click()
+        await this.increaseButton.click()
+    }
+
 }
 
 export default new NavigationDropdown();
